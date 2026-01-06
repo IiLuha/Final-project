5186fdd95dfa856a8627bfc9f700663d6e621f19
IiLuha <elyamin@bk.ru>
2026-01-06 16:23:53 +0300

    Added dependencies and properties

--- src/test/java/com/itdev/finaproject/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.itdev.finaproject;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
